141bdb2d0221ad5de132f990f2ce43e8fdcd15f6
yunus.emre.kepenek <yunus.kepenek@dt.net.tr>
2023-08-06 00:51:38 +0300

    Setup blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					100: "#B7E8CD",
+					200: "#85A895",
+					300: "#7B9C8A",
+					400: "#678273",
+					500: "#495C51",
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
