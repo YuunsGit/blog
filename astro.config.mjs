c77a91f29e5c9968adcb41a6993eedbb12f3a375
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-08-06 21:32:53 +0300

    Responsive design with Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
-import {remarkReadingTime} from "./src/remark-reading-time.mjs";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.yuuns.tech',
+  site: "https://blog.yuuns.tech",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
+  output: "static",
+  adapter: vercel({
+    analytics: true,
+  })
 });
