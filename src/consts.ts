ccdfa1f063da57635c078ad097c880c5fbc2f265
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-08-08 20:50:41 +0300

    SVG in React blog post

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Personal Blog by Yunus Emre Kepenek';
-export const SITE_DESCRIPTION = 'Welcome to my personal blog.';
+export const SITE_DESCRIPTION = 'Welcome to my personal thumbnails.';
