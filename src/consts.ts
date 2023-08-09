af6bcb40c9b7321ddf3a0c21bb033ada2b8f014a
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-08-10 00:01:16 +0300

    Update metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Personal Blog by Yunus Emre Kepenek';
-export const SITE_DESCRIPTION = 'Welcome to my personal thumbnails.';
+export const SITE_DESCRIPTION = 'My personal blog where I post regularly.';
