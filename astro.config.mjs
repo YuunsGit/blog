c7d4dadc97e32fb63572b9ed3f4227dbcc6b88dc
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-08-12 00:11:16 +0300

    Migrate to sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig, sharpImageService} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./src/remark-reading-time.mjs";
-
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
@@ -19,5 +18,8 @@ export default defineConfig({
   }),
   experimental: {
     assets: true
+  },
+  image: {
+    service: sharpImageService()
   }
 });
